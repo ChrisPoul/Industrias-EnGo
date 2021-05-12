32458fd57dd67225b115086057f2f91a27e6568f
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-05-12 12:56:12 -0500

    Set basic layout

---
+++ EnGo/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(
+        __name__,
+        instance_relative_config=True
+    )
+    app.secret_key = "dev"
+
+    return app
