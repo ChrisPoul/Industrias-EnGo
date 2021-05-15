a04c99ec4251ca40cc38eaba275619e7cbfcbe62
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-05-15 13:58:04 -0500

    Changed some names

--- EnGo/views/auth.py
+++ EnGo/views/auth.py
@@ -2,17 +2,16 @@ from flask import (
     Blueprint, render_template, request,
     flash, redirect, url_for
 )
-from EnGo.models.user.auth import Auth
+from EnGo.models.user.auth import UserAuth
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
-auth = Auth()
+auth = UserAuth()
 
 
 @bp.route("/login", methods=('POST', 'GET'))
 def login():
     if request.method == "POST":
         error = auth.login()
-
         if not error:
             return redirect(
                 url_for('home.main_page')
