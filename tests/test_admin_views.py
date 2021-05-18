4322edaf2681f5eb127a2a6da695bb33fbb6aa54
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-05-18 18:39:31 -0500

    Refactored tests using login_user method

--- tests/test_admin_views.py
+++ tests/test_admin_views.py
@@ -8,28 +8,13 @@ class AdminViewTest(Test):
 
     def setUp(self):
         Test.setUp(self)
-        self.admin_permission = Permission(
-            permission_name="admin",
-        )
-        self.admin_permission.add()
-        self.admin_user = User(
-            username="Admin User",
-            password="0000"
-        )
-        self.admin_user.add()
-        self.admin_user.add_permission(self.admin_permission)
-        self.normal_user = User(
-            username="Normal User",
-            password="0000"
-        )
-        self.normal_user.add()
+        self.create_test_users()
 
 
 class TestMainPage(AdminViewTest):
 
     def test_should_grant_access_given_loged_in_user_is_admin(self):
-        with self.client.session_transaction() as session:
-            session["user_id"] = self.admin_user.id
+        self.login_user(self.admin_user)
         response = self.client.get(
             url_for('admin.main_page')
         )
@@ -37,8 +22,7 @@ class TestMainPage(AdminViewTest):
         self.assert200(response)
 
     def test_should_return_redirect_given_loged_in_user_is_not_admin(self):
-        with self.client.session_transaction() as session:
-            session["user_id"] = self.normal_user.id
+        self.login_user(self.normal_user)
         response = self.client.get(
             url_for('admin.main_page')
         )
