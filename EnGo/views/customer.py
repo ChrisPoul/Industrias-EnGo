aa6525a80f6c221bbf5d816ad0fea47e51c358d4
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-05-18 16:47:10 -0500

    Added customer views with corresponding tests

--- EnGo/views/customer.py
+++ EnGo/views/customer.py
@@ -1,10 +1,24 @@
 from flask import (
     Blueprint, render_template, request,
-    flash
+    flash, redirect, url_for
 )
+from EnGo.models import customer
 from EnGo.models.customer import Customer
 
 bp = Blueprint('customer', __name__, url_prefix='/customer')
+
+customer_heads = dict(
+    customer_name='Nombre del cliente',
+    address="Dirección",
+    rfc="RFC"
+)
+
+
+@bp.route('/customers')
+def customers():
+    return render_template(
+        'customer/customers.html'
+    )
 
 
 @bp.route('/add', methods=("POST", "GET"))
@@ -21,3 +35,29 @@ def add():
     return render_template(
         'customer/add.html'
     )
+
+
+@bp.route('/update/<int:id>', methods=("POST", 'GET'))
+def update(id):
+    customer = Customer.get(id)
+    if request.method == "POST":
+        for attribute in customer_heads:
+            setattr(customer, attribute, request.form[attribute])
+        error = customer.request.update()
+        flash(error)
+
+    return render_template(
+        'customer/update.html',
+        customer=customer
+    )
+
+
+@bp.route('/delete/<int:id>')
+def delete(id):
+    customer = Customer.get(id)
+    customer.delete()
+
+    return redirect(
+        url_for('customer.customers')
+    )
+    
--- tests/test_customer_views.py
+++ tests/test_customer_views.py
@@ -15,6 +15,17 @@ class CustomerViewTest(Test):
         self.customer.add()
 
 
+class TestCustomersView(CustomerViewTest):
+
+    def test_should_return_valid_response(self):
+        with self.client as client:
+            response = client.get(
+                url_for('customer.customers')
+            )
+        
+        self.assert200(response)
+
+
 class TestAddView(CustomerViewTest):
 
     def test_should_add_customer_given_valid_customer_data(self):
@@ -59,6 +70,41 @@ class TestUpdateView(CustomerViewTest):
                 url_for('customer.update', id=self.customer.id),
                 data=customer_data
             )
+        self.db.session.rollback()
         
         self.assertEqual(self.customer.customer_name, "New Name")
+
+    def test_should_not_update_customer_given_invalid_customer_data(self):
+        customer_data = dict(
+            customer_name="", 
+            address="Valid Address",
+            rfc=""
+        )
+        with self.client as client:
+            client.post(
+                url_for('customer.update', id=self.customer.id),
+                data=customer_data
+            )
+        self.db.session.rollback()
+
+        self.assertNotEqual(self.customer.customer_name, "")
+
+class DeleteCustomerView(CustomerViewTest):
+
+    def test_should_delete_customer(self):
+        with self.client as client:
+            client.get(
+                url_for('customer.delete', id=self.customer.id)
+            )
+        
+        self.assertNotIn(self.customer, self.db.session)
+    
+    def test_should_redirect(self):
+        with self.client as client:
+            response = client.get(
+                url_for('customer.delete', id=self.customer.id)
+            )
+
+        self.assertStatus(response, 302)
+
 
