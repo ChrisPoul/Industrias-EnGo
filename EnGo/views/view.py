d3342f53444ceea02f1dc9dba34eede51df7d5c0
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-06-02 13:55:23 -0500

    Now the receipt image can be configured from receipt.edit config view

--- EnGo/views/view.py
+++ EnGo/views/view.py
@@ -1,9 +1,11 @@
+import os
 from flask import (
     Blueprint, render_template, request,
-    redirect, url_for, session
+    redirect, url_for, session, current_app
 )
 from EnGo.models.permission import Permission
 from EnGo.models.view import View
+from EnGo.commands import get_settings, save_settings
 from . import get_checked_permissions
 
 
@@ -14,9 +16,11 @@ bp = Blueprint('view', __name__, url_prefix="/view")
 def update(id):
     view = View.get(id)
     if request.method == "POST":
+        receipt_image = request.files["receipt_image"]
+        if receipt_image:
+            save_image(receipt_image)
         checked_permissions = get_checked_permissions()
         view.update_permissions(checked_permissions)
-        print(view.view_permissions)
         try:
             url = session["prev_url"]
         except KeyError:
@@ -30,3 +34,12 @@ def update(id):
         'view/update.html',
         view=view
     )
+
+
+def save_image(image_file):
+    receipt_image = os.path.join("images", image_file.filename)
+    image_path = os.path.join(current_app.static_folder, receipt_image)
+    image_file.save(image_path)
+    settings = get_settings()
+    settings["receipt_image"] = receipt_image
+    save_settings(settings)
