e56cccf6bb4728be8671a5bb18bdc12de9851518
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-06-02 02:29:54 -0500

    Added error handling

--- EnGo/models/receipt/__init__.py
+++ EnGo/models/receipt/__init__.py
@@ -56,12 +56,16 @@ class Receipt(db.Model, MyModel):
         return [sold_product.product for sold_product in self.sold_products]
 
     def add_product(self, product):
+        try:
+            unit = product.unit
+        except AttributeError:
+            unit = "pz"
         from EnGo.models.product import SoldProduct
         sold_product = SoldProduct(
             receipt_id=self.id,
             product_id=product.id,
             price=product.price,
-            unit=product.unit
+            unit=unit
         )
         sold_product.add()
 
