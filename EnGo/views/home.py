118e6a13f4b9f82678463d514e76f31ca1f0541b
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-06-03 16:03:29 -0500

    Added html to post views

--- EnGo/views/home.py
+++ EnGo/views/home.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, render_template
 )
+from EnGo.models.post import Post
 from . import login_required
 
 bp = Blueprint('home', __name__)
@@ -9,7 +10,9 @@ bp = Blueprint('home', __name__)
 @bp.route('/')
 @login_required
 def main_page():
+    posts = Post.get_all()
 
     return render_template(
-        'home/main-page.html'
+        'home/main-page.html',
+        posts=posts
     )
