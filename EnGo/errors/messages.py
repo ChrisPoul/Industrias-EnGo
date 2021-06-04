999ddbccf51df09ca17113f452d18b05aa92b22b
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-06-04 15:02:29 -0500

    Added error message for invalid number

--- EnGo/errors/messages.py
+++ EnGo/errors/messages.py
@@ -1,3 +1,4 @@
 empty_value_error = "No se pueden dejar campos vacíos"
 repeated_value_error = "Eso no está disponible"
 invalid_phone_error = "Número telefónico invalido"
+invalid_num_error = "Número invalido"
