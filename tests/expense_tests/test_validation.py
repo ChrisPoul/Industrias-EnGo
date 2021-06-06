5afb3e115191cb863aaf588dff7d1f22a50281f1
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-06-06 00:18:49 -0500

    Fixed incomplete tests

--- tests/expense_tests/test_validation.py
+++ tests/expense_tests/test_validation.py
@@ -2,7 +2,7 @@ from . import ExpenseTest
 from EnGo.models.expense import Expense
 
 
-class TestValidateEmptyValues(ExpenseTest):
+class TestValidate(ExpenseTest):
 
     def test_should_not_return_error_given_valid_expense(self):
         expense = Expense(
@@ -10,6 +10,28 @@ class TestValidateEmptyValues(ExpenseTest):
             type_id=self.expense_type.id
         )
         error = expense.validation.validate()
+
+        self.assertEqual(error, None)
+    
+    def test_should_return_error_given_invalid_expense(self):
+        expense = Expense(
+            concept="",
+            type_id=self.expense_type.id,
+            cost="invalid cost"
+        )
+        error = expense.validation.validate()
+
+        self.assertNotEqual(error, None)
+
+
+class TestValidateEmptyValues(ExpenseTest):
+
+    def test_should_not_return_error_given_no_empty_values(self):
+        expense = Expense(
+            concept="Valid Name",
+            type_id=self.expense_type.id
+        )
+        error = expense.validation.validate_empty_values()
 
         self.assertEqual(error, None)
     
@@ -18,19 +40,19 @@ class TestValidateEmptyValues(ExpenseTest):
             concept="",
             type_id=self.expense_type.id
         )
-        error = expense.validation.validate()
+        error = expense.validation.validate_empty_values()
 
         self.assertNotEqual(error, None)
 
 
 class TestValidateUniqueValues(ExpenseTest):
 
-    def test_should_not_return_error_given_valid_expense(self):
+    def test_should_not_return_error_given_unique_expense(self):
         expense = Expense(
             concept="Valid Name",
             type_id=self.expense_type.id
         )
-        error = expense.validation.validate()
+        error = expense.validation.validate_unique_values()
 
         self.assertEqual(error, None)
 
@@ -39,11 +61,34 @@ class TestValidateUniqueValues(ExpenseTest):
             concept="Test Expense",
             type_id=self.expense_type.id
         )
-        error = expense.validation.validate()
+        error = expense.validation.validate_unique_values()
 
         self.assertNotEqual(error, None)
     
     def test_should_not_return_error_given_expense_already_in_db(self):
-        error = self.expense.validation.validate()
+        error = self.expense.validation.validate_unique_values()
 
         self.assertEqual(error, None)
+
+
+class TestValidateNums(ExpenseTest):
+
+    def test_should_not_return_error_given_valid_nums(self):
+        expense = Expense(
+            concept="Test Expense",
+            type_id=self.expense_type.id,
+            cost="10"
+        )
+        error = expense.validation.validate_nums()
+
+        self.assertEqual(error, None)
+
+    def test_should_return_error_given_invalid_nums(self):
+        expense = Expense(
+            concept="Test Expense",
+            type_id=self.expense_type.id,
+            cost="invalid_num"
+        )
+        error = expense.validation.validate_nums()
+
+        self.assertNotEqual(error, None)
