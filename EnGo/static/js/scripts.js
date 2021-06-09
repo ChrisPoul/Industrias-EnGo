d9a0319d47df16c66326a8b80f21045c7bb351ec
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-06-09 14:22:33 -0500

    Styled the inventory table

---
+++ EnGo/static/js/scripts.js
@@ -0,0 +1,9 @@
+function on() {
+    document.getElementById("product-inventory").style.display = "block";
+    document.getElementById("overlay").style.display = "block" 
+}
+  
+function off() {
+    document.getElementById("product-inventory").style.display = "none";
+    document.getElementById("overlay").style.display = "none" 
+}
