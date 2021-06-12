3c9a899d01a35328e68813560902f2e6e4394c53
Farouk Bulhosen <farouk_jr@hotmail.com>
2021-06-11 21:09:38 -0500

    Fixed dropdown menu with javascript

--- EnGo/static/js/scripts.js
+++ EnGo/static/js/scripts.js
@@ -7,3 +7,19 @@ function off() {
     document.getElementById("product-inventory").style.display = "none";
     document.getElementById("overlay").style.display = "none" 
 }
+
+window.onclick = event => {
+    element = event.target
+    elementClass = element.className
+    if (elementClass == "dropbtn") {
+        dropdown = element.parentElement
+        dropdown_content = dropdown.children[1]
+        dropdown_content.style.display = "block" 
+    }
+    else {
+        dropdowns = document.getElementsByClassName("dropdown-content")
+        for(i = 0; i < dropdowns.length; i++){
+            dropdowns[i].style.display = "none" 
+        }
+    }
+}
