f8a95dc8bb18c4cb0265572f93e18249fd623e84
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-06-15 17:15:57 -0500

    Added get_time_elapsed function with corresponding tests

--- EnGo/models/user/contract.py
+++ EnGo/models/user/contract.py
@@ -15,5 +15,34 @@ class Contract(db.Model, MyModel):
 
     @property
     def duration(self):
-        return 1
+        if not self.end:
+            return None
+        duration = self.end - self.start
 
+        return duration.days
+
+    @property
+    def seniority(self):
+        seniority = datetime.today() - self.start
+
+        return seniority.days
+
+    @property
+    def vacation_days(self):
+        vacation_days = 6
+
+        return vacation_days
+
+
+def get_elapsed_time(start_date, end_date, time_period="days"):
+    if time_period == "days":
+        elapsed_time = (end_date - start_date).days
+    elif time_period == "years":
+        elapsed_time = end_date.year - start_date.year
+    elif time_period == "months":
+        elapsed_years = end_date.year - start_date.year
+        elapsed_time = end_date.month - start_date.month + (elapsed_years * 12)
+    else:
+        raise ValueError("Invalid Time Period")
+
+    return elapsed_time
--- tests/user_tests/test_contract.py
+++ tests/user_tests/test_contract.py
@@ -1,24 +1,64 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from . import UserTest
-from EnGo.models.user import Contract
+from EnGo.models.user.contract import Contract, get_elapsed_time
 
 
 class TestContract(UserTest):
 
     def setUp(self):
         UserTest.setUp(self)
-        self.contract = Contract(
+        contract = Contract(
             user_id=self.user.id,
             type="Contract Type",
             start=datetime(2020, 1, 1),
             end=datetime(2021, 1, 1)
         )
-        self.contract.add()
+        contract.add()
     
 
 class TestDuration(TestContract):
 
-    def test_should_return_duration_in_days_given_start_and_end_dates(self):
-        self.contract.start = datetime(2021, 1, 1)
-        self.contract.end = datetime(2021, 1, 2)
-        self.assertEqual(self.user.contract.duration, 1)
+    def test_should_return_duration_in_days_given_contract_end_date(self):
+        self.user.contract.start = datetime(2021, 1, 1)
+        self.user.contract.end = datetime(2021, 1, 10)
+
+        self.assertEqual(self.user.contract.duration, 9)
+
+    def test_should_return_none_given_indefinite_contract(self):
+        self.user.contract.end = None
+
+        self.assertEqual(self.user.contract.duration, None)
+
+
+class TestSeniority(TestContract):
+
+    def test_should_return_days_passed_since_start_of_contract_up_to_today(self):
+        start_date = datetime(2021, 6, 1)
+        self.user.contract.start = start_date
+        elapsed_time_up_to_today = datetime.today() - start_date
+
+        self.assertEqual(self.user.contract.seniority, elapsed_time_up_to_today.days)
+
+
+class VacationDays(TestContract):
+
+    def test_should_return_6_days_given_less_than_a_year_since_start_of_contract(self):
+        self.user.contract.start = datetime.today() - timedelta(days=360)
+        
+        self.assertEqual(self.user.contract.vacation_days, 6)
+
+    def test_should_return_12_given_four_years_or_less_since_start_of_contract(self):
+        self.user.contract.start = datetime.today() - timedelta(days=4*360)
+        
+        self.assertEqual(self.user.contract.vacation_days, 12)
+
+
+class TestGetElapsedTime(TestContract):
+
+    def test_should_return_years_elapsed_between_two_dates_given_years_as_parameter(self):
+        elapsed_years = get_elapsed_time(datetime(2020, 1, 1), datetime(2020, 12, 30), "years")
+
+        self.assertEqual(elapsed_years, 0)
+
+    def test_should_return_months_elapsed_between_two_dates_given_months_as_parameter(self):
+        
