c3737b0a4e268fba253a6382b225b3bcdc3a7ea4
ChrisPoul <chrispoulsenfernandez@gmail.com>
2021-08-05 17:10:04 -0500

    Refactored activity

--- EnGo/views/activity.py
+++ EnGo/views/activity.py
@@ -3,6 +3,7 @@ from flask import (
     Blueprint, render_template, request,
     flash, redirect, url_for
 )
+from EnGo.models.user import User
 from EnGo.models.activity import Activity
 from EnGo.views import (
     permission_required, login_required,
@@ -16,6 +17,10 @@ activity_heads = dict(
     description="Descripción",
     user_id="Empleado"
 )
+initial_user = dict(
+    id=0,
+    username="Seleccionar Empleado"
+)
 permissions = [
     "Recursos Humanos"
 ]
@@ -25,6 +30,8 @@ permissions = [
 @permission_required(permissions)
 @login_required
 def assign():
+    users = [user for user in User.query.all() if not user.is_admin()]
+    selected_user = initial_user
     if request.method == "POST":
         activity = Activity(
             user_id=request.form['user_id'],
@@ -40,7 +47,9 @@ def assign():
         
     return render_template(
         "activity/assign.html",
-        activity_heads=activity_heads
+        activity_heads=activity_heads,
+        selected_user=selected_user,
+        users=users
     )
 
 
@@ -49,6 +58,8 @@ def assign():
 @login_required
 def update(activity_id):
     activity = Activity.query.get(activity_id)
+    users = [user for user in User.query.all() if not user.is_admin()]
+    selected_user = activity.user
     if request.method == "POST":
         update_obj_attrs(activity, activity_heads)
         error = activity.request.update()
@@ -61,5 +72,7 @@ def update(activity_id):
     return render_template(
         'activity/update.html',
         activity_heads=activity_heads,
+        selected_user=selected_user,
+        users=users,
         activity=activity
     )
